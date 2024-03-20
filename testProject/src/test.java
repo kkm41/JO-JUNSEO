34028380cdcf5c4ef79deed3eabdc2156709928d
DESKTOP-F4BC13L\hi <ssbf41@gmail.com>
2024-03-20 10:05:01 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+
+
+        System.out.println("안녕하세요!");
+        System.out.println("하이");
+        System.out.print("하이");
+        System.out.print("하이");
+
+    }
+}
---
+++ testProject/src/test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+    public static void main(String[] args){
+        System.out.print("이것은 연습");
+        System.out.println("이것도 연습");
+        System.out.println("이렇게 치면 떨어져");
+    }
+
+}
