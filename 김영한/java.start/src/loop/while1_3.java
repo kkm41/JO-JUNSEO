f2a9a6a7ccf740866e0444bb480a29b65e9f4246
JJS <163955185+kkm41@users.noreply.github.com>
2024-04-02 18:21:23 +0900

    Add files via upload

---
+++ 김영한/java.start/src/loop/while1_3.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class while1_3 {
+    public static void main(String[] args) {
+        int num = 0;
+        int i = 1;
+        int endNum =10;
+
+
+        while(i<=endNum){
+            num = num + i;
+            System.out.println(num);
+            i++;
+        }
+
+
+    }
+}
